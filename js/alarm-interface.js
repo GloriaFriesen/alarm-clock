dff5032c2e8cfc2f8ea70c6b86af1769828e3e2e
Chris Carr and Gloria Friesen <gloria@>
2017-04-18 16:04:17 -0700

    add snooze functionality

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -4,13 +4,23 @@ var alarmOn = function() {
   var date = moment();
   if (alarmTime === date.format('HH:mm')) {
     $("#wakeUp").text("Alarm is currently going off.");
+    $("#snooze").show();
   }
-}
-
+};
 
 $(document).ready(function() {
   $("#alarm").click(function() {
     alarmTime = $("#alarmInput").val();
+    alarmOn();
+    setInterval(alarmOn, 1000);
+  });
+
+  $("#snoozeButton").click(function() {
+    $("#wakeUp").text("You are currently snoozing.");
+    $("#snooze").hide();
+    var snoozeTime = moment();
+    alarmTime = snoozeTime.add(1, 'minutes').format('HH:mm');
+    console.log("alarmTime: " + alarmTime);
     alarmOn();
     setInterval(alarmOn, 1000);
   });
--- js/time-interface.js
+++ js/time-interface.js
@@ -4,7 +4,7 @@ var date = null;
 var update = function() {
   date = moment();
   datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-}
+};
 
 $(document).ready(function(){
   datetime = $('#time');

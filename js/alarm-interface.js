8ced6a3ee35face5179f2004e09f76124789c2d7
Chris Carr and Gloria Friesen <chris@>
2017-04-18 16:49:22 -0700

    add stop alarm button

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -5,12 +5,14 @@ var alarmOn = function() {
   if (alarmTime === date.format('HH:mm')) {
     $("#wakeUp").text("Alarm is currently going off.");
     $("#snooze").show();
+    $("#stop").show();
   }
 };
 
 $(document).ready(function() {
   $("#alarm").click(function() {
     alarmTime = $("#alarmInput").val();
+    $("#wakeUp").text("Your alarm is set for " + alarmTime);
     alarmOn();
     setInterval(alarmOn, 1000);
   });
@@ -19,9 +21,17 @@ $(document).ready(function() {
     $("#wakeUp").text("You are currently snoozing.");
     $("#snooze").hide();
     var snoozeTime = moment();
-    alarmTime = snoozeTime.add(1, 'minutes').format('HH:mm');
+    var snoozeMinutes = parseInt($("#snoozeInput").val());
+    alarmTime = snoozeTime.add(snoozeMinutes, 'minutes').format('HH:mm');
     console.log("alarmTime: " + alarmTime);
     alarmOn();
     setInterval(alarmOn, 1000);
+  });
+
+//stop button only works after alarm has been on for 1 minute (4:45 = 4:45; 4:45 != 4:46)
+  $("#stopButton").click(function() {
+    $("#wakeUp").empty();
+    $("#snooze").hide();
+    $("#stop").hide();
   });
 });
